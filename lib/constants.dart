f1cf60017236ddcd54029df0272f36b1bfc854c4
Maik96 <maikorenner96@gmail.com>
2023-12-01 01:02:12 +0100

    Adding Constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,12 @@
+// Strings
+
+import 'package:flutter/material.dart';
+
 const name = 'Maik Renner';
 const desc = 'Flutter Developer';
 const email = 'coding.maikorenner@gmail.com';
+
+// Colors
+
+Color black = const Color.fromARGB(255, 16, 16, 16);
+Color white = const Color.fromARGB(255, 228, 228, 228);
